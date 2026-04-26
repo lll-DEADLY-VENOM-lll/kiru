1d7cdb72222686539a69e34102847c78d52cf95a
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-04-26 08:50:43 +0530

    Add additional help buttons to inline keyboard

--- kiru/utils/inline/help.py
+++ kiru/utils/inline/help.py
@@ -72,6 +72,20 @@ def help_pannel(_, START: Union[bool, int] = None):
                     callback_data="help_callback hb12",
                 ),
             ],
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_13"],
+                    callback_data="help_callback hb13",
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_14"],
+                    callback_data="help_callback hb14",
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_15"],
+                    callback_data="help_callback hb15",
+                ),
+            ],
             mark,
         ]
     )
